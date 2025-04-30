be7cee0f9ab7f48c7e0d6c17fbb129ff7f907f73
Renê de Souza Pinto <rene@renesp.com.br>
2025-04-30 07:10:47 +0200

    Fix make targets to take LINUXKIT_ORG_TARGET into account
    LINUXKIT_ORG_TARGET was not taking into account on some make targets and
    by the tools/makerootfs.sh and tools/parse-pkgs.sh scripts. Even when using
    linuxkit show-tag, it's important to take LINUXKIT_ORG_TARGET into account.
    Otherwise, make eve will fail when using a custom REGISTRY value.
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- tools/makeconfig.sh
+++ tools/makeconfig.sh
@@ -5,7 +5,7 @@
 #
 EVE="$(cd "$(dirname "$0")" && pwd)/../"
 PATH="$EVE/build-tools/bin:$PATH"
-MKCONFIG_TAG="$(linuxkit pkg show-tag "$EVE/pkg/mkconf")"
+MKCONFIG_TAG="$(linuxkit pkg ${LINUXKIT_ORG_TARGET} show-tag "$EVE/pkg/mkconf")"
 IMAGE="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 ROOTFS_VERSION="$2"
 
